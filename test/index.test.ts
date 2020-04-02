bda2f2c124a3ab404dcf5077ac7a3545548edfb1
Pedro Gomes <pedrogomes94@gmail.com>
2020-04-02 15:42:23 +0200

    clean console.log

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,15 +4,13 @@ describe('blockies', () => {
   it('should generate from ethereum address', () => {
     const address = '0x8B7B2b4F7A391b6f14A81221AE0920a9735B67Fc';
     const imgSrc = blockies.create({ seed: address }).toDataURL();
-    console.log('imgSrc', imgSrc);
     expect(imgSrc).toBeTruthy();
   });
 });
 
 describe('utils', () => {
-  it('should create image date successfully', () => {
+  it('should create image data successfully', () => {
     const imgData = blockies.createImageData(blockies.DEFAULT_SIZE);
-    console.log('imgData', imgData);
     expect(imgData).toBeTruthy();
   });
 });
