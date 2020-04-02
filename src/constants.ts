342ed2d405928bf020bbd87471da1d2e40cd4218
Pedro Gomes <pedrogomes94@gmail.com>
2020-04-02 15:41:05 +0200

    refactor

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_SIZE = 8;
+export const DEFAULT_SCALE = 4;
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface BlockiesOptions {
+  seed: string;
+  size: number;
+  scale: number;
+  color: string;
+  bgcolor: string;
+  spotcolor: string;
+}
